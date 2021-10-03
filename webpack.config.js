691ffa6fa756570b2ce1067176faa6179f0f05ef
mohamed karara <mohamed.karara@metronom.com>
2021-10-04 01:12:19 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
