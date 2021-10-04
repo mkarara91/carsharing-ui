44c89da5096af387498b81037797bc6286a08872
mohamed karara <mohamed.karara@metronom.com>
2021-10-04 23:54:29 +0200

    style check task

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,15 @@ import Home from "./components/home";
 import "./style.scss";
 
 function App() {
-  const [state, setState] = useState("CLICK ME");
+    const [state, setState] = useState("CLICK ME");
 
-  return (
-    <div>
-      <h1>WELCOME</h1>
-      <Home />
-      <button onClick={() => setState("CLICKED")}>{state}</button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>WELCOME</h1>
+            <Home />
+            <button onClick={() => setState("CLICKED")}>{state}</button>
+        </div>
+    );
 }
 
 render(<App />, document.getElementById("root"));
