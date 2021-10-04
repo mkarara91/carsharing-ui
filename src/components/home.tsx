dc45759917aa7bbe037202207bc0797b575f05ac
mohamed karara <mohamed.karara@metronom.com>
2021-10-04 22:25:33 +0200

    changed title

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="main-wrapper">React Application</div>;
+  return <div className="main-wrapper">Car Pooling APP</div>;
 };
 
 export default Home;
