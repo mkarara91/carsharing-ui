159425398ad2b8e9f4e6ecac8ecbdb6fdb768bc5
mohamed karara <mohamed.karara@metronom.com>
2021-10-05 13:23:08 +0200

    added address fields

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,26 @@
 import * as React from "react";
 import "./home.scss";
+import DestinationAddressSearchInput from "./map/inputs/DestinationAddressInput";
+import StartAddressSearchInput from "./map/inputs/StartAddressSearchInput";
 
 const Home = () => {
-    return <div className="main-wrapper">Car Pooling APP 2</div>;
+    return (
+        <div className="main-wrapper">
+            <h1>Car Pooling APP</h1>
+
+            <div className="address-input-container">
+                <div className="start-input">
+                    <h3>Enter Start Address:</h3>
+                    <StartAddressSearchInput/>
+                </div>
+                <div className="destination-input">
+                    <h4>Enter Destination Address:</h4>
+                    <DestinationAddressSearchInput/>
+                </div>
+                
+            </div>
+            
+        </div>);
 };
 
 export default Home;
---
+++ src/components/map/inputs/StartAddressSearchInput.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete"
+
+const StartAddressSearchInput = () => {
+    return (
+        <div>
+            <GooglePlacesAutocomplete apiKey=""/>
+        </div>
+    );
+}
+
+export default StartAddressSearchInput;
